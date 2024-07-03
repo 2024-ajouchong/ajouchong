42be1c180534a738886648c6561c35c25a68ae86
ytnwjd <ysj120808@gmail.com>
2024-07-03 20:26:49 +0900

    git init

---
+++ src/main/java/com/ajouchong/ajouchong/AjouchongApplication.java
@@ -0,0 +1,13 @@
+package com.ajouchong.ajouchong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouchongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjouchongApplication.class, args);
+	}
+
+}
