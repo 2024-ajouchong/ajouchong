6128c03b6ea188eb78c71d0d22df0521227bce77
ytnwjd <ysj120808@gmail.com>
2024-07-19 23:01:10 +0900

    feat: add userRepositoryInterface

--- src/main/java/com/ajouchong/repository/UserRepositoryInterface.java
+++ src/main/java/com/ajouchong/repository/UserRepositoryInterface.java
@@ -1,2 +1,11 @@
-package com.ajouchong.repository;public interface UserRepositoryInterface {
+package com.ajouchong.repository;
+
+import com.ajouchong.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepositoryInterface {
+    void save(User user);
+    Optional<User> findById(String id);
+    Optional<User> findByName(String name);
 }
