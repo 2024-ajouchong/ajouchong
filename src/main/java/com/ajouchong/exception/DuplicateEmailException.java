8fe1084d9c5e8cc35cb0904e6e2804ca5ee4eae3
ytnwjd <ysj120808@gmail.com>
2024-07-19 19:41:47 +0900

    feat: make exception

--- src/main/java/com/ajouchong/exception/DuplicateEmailException.java
+++ src/main/java/com/ajouchong/exception/DuplicateEmailException.java
@@ -1,2 +1,7 @@
-package com.ajouchong.exception;public class DuplicateEmailException {
+package com.ajouchong.exception;
+
+public class DuplicateEmailException extends RuntimeException{
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
 }
