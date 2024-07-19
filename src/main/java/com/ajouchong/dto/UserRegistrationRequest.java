3e2fff99e6b53d9bba7facbae5d14ddb5c74d771
ytnwjd <ysj120808@gmail.com>
2024-07-19 21:31:02 +0900

    change: NotBlank to NotNull

--- src/main/java/com/ajouchong/dto/UserRegistrationRequest.java
+++ src/main/java/com/ajouchong/dto/UserRegistrationRequest.java
@@ -3,6 +3,7 @@ package com.ajouchong.dto;
 import com.ajouchong.entity.UserRole;
 import com.ajouchong.entity.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -35,7 +36,7 @@ public class UserRegistrationRequest {
     @NotBlank(message = "학과를 선택해주세요.")
     private String major;
 
-    @NotBlank(message = "일반 학생/학생회를 선택해주세요.")
+    @NotNull(message = "일반 학생/학생회를 선택해주세요.")
     private UserRole role;
 
     public User toEntity() {;
