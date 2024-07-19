48e85d28dc8f160d3289d943ae04308af49533c0
ytnwjd <ysj120808@gmail.com>
2024-07-19 22:57:39 +0900

    chore: rename user field

--- src/main/java/com/ajouchong/service/UserService.java
+++ src/main/java/com/ajouchong/service/UserService.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserService {
     User join(UserRegistrationRequestDto requestDto);   //회원 가입
-    Optional<User> findUser(String u_name);
-    Optional<User> getCurrentUser();//아이디로 회원 검색
+    Optional<User> findUser(String name);
+    Optional<User> getCurrentUser();
 }
