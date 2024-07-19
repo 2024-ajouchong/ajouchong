a048a1638ce498a2f9a5e3a812741cfb870f4bd2
ytnwjd <ysj120808@gmail.com>
2024-07-19 20:03:47 +0900

    feat: securityConfig

--- src/main/java/com/ajouchong/config/SecurityConfig.java
+++ src/main/java/com/ajouchong/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ajouchong.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -19,6 +21,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/auth/**", "/public/**").permitAll()
+                                .requestMatchers("/api/member/mypage").authenticated() // 마이페이지 API는 인증 필요
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
@@ -47,5 +50,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
