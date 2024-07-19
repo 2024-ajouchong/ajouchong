69a482e1e83b0ad968e05d5c509b529c3cbb81f7
ytnwjd <ysj120808@gmail.com>
2024-07-19 19:58:11 +0900

    feat: ApiResponse

---
+++ src/main/java/com/ajouchong/common/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.ajouchong.common;
+
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public ApiResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
