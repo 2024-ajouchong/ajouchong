757e80ee13433dd378bea3d4d9681efb0d3e80c9
ytnwjd <ysj120808@gmail.com>
2024-07-19 21:22:11 +0900

    chore: add NonNull

--- src/main/java/com/ajouchong/repository/UserRepository.java
+++ src/main/java/com/ajouchong/repository/UserRepository.java
@@ -2,16 +2,19 @@ package com.ajouchong.repository;
 
 import com.ajouchong.entity.User;
 import com.ajouchong.entity.UserRole;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+    @NonNull
+    Optional<User> findById(@NonNull String id);
     Optional<User> findByEmail(String email);
     Optional<User> findByName(String name);
     Optional<User> findByMajor(String major);
-    Optional<User> findById(String id);
     Optional<User> findByRole(UserRole role);
 
     boolean existsByEmail(String email);
