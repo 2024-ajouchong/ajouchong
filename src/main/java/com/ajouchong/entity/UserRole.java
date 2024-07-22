bba74300ffd6780a366d887fb91fd67458c7e886
ytnwjd <ysj120808@gmail.com>
2024-07-23 00:31:20 +0900

    chore: impl grantedAuth

--- src/main/java/com/ajouchong/entity/UserRole.java
+++ src/main/java/com/ajouchong/entity/UserRole.java
@@ -1,7 +1,14 @@
 package com.ajouchong.entity;
 
-public enum UserRole {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
     ADMIN,
     STUDENT,
-    COUNCIL
+    COUNCIL;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
