f238baa15eb9179624aebb1f89e517ca8c036ebc
ytnwjd <ysj120808@gmail.com>
2024-07-23 14:46:09 +0900

    chore: delete import on fly

--- src/main/java/com/ajouchong/config/SpringConfig.java
+++ src/main/java/com/ajouchong/config/SpringConfig.java
@@ -1,15 +1,12 @@
 package com.ajouchong.config;
 
 import com.ajouchong.jwt.JwtTokenProvider;
-import com.ajouchong.repository.MemoryUserRepository;
 import com.ajouchong.repository.UserRepository;
 import com.ajouchong.service.UserService;
 import com.ajouchong.service.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
