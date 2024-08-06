980b06088ce85a6419927d9a690d4a03056ef756
ytnwjd <ysj120808@gmail.com>
2024-08-07 02:39:45 +0900

    chore: add annotation allArgs

--- src/main/java/com/ajouchong/common/ApiResponse.java
+++ src/main/java/com/ajouchong/common/ApiResponse.java
@@ -1,18 +1,14 @@
 package com.ajouchong.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ApiResponse<T> {
     private int code;
     private String message;
     private T data;
-
-    public ApiResponse(int code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 }
