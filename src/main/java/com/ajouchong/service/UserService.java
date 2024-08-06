c039cdf891a7fceba2bc5ce43fe7d9afacd1a9a7
ytnwjd <ysj120808@gmail.com>
2024-08-06 21:36:51 +0900

    build: add thymeleaf

--- src/main/java/com/ajouchong/service/UserService.java
+++ src/main/java/com/ajouchong/service/UserService.java
@@ -1,15 +1,2 @@
-package com.ajouchong.service;
-
-import com.ajouchong.dto.UserRegistrationRequestDto;
-import com.ajouchong.dto.UserRegistrationResponseDto;
-import com.ajouchong.entity.User;
-import com.ajouchong.jwt.JwtTokenDto;
-
-import java.util.Optional;
-
-public interface UserService {
-    UserRegistrationResponseDto save(UserRegistrationRequestDto requestDto);   //회원 가입
-    Optional<User> findUser(String name);
-    Optional<User> getCurrentUser();
-    JwtTokenDto signIn(String username, String password);
+package com.ajouchong.service;public class UserService {
 }
