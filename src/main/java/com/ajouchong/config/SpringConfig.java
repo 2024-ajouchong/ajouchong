4462057f8abe5474350cbc44a7c1395fa2d7bd1d
ytnwjd <ysj120808@gmail.com>
2024-08-09 23:17:02 +0900

    chore: add springConfig

--- src/main/java/com/ajouchong/config/SpringConfig.java
+++ src/main/java/com/ajouchong/config/SpringConfig.java
@@ -1,2 +1,15 @@
-package com.ajouchong.config;public class SpringConfig {
+package com.ajouchong.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
