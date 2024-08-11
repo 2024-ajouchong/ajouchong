7ec863c32f5738a1cc917bad8bfe1fd154590381
유수정 <ysj120808@gmail.com>
2024-08-11 20:07:18 +0900

    chore: #2 duplication email check

--- src/main/java/com/ajouchong/controller/MemberController.java
+++ src/main/java/com/ajouchong/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ajouchong.controller;
 import com.ajouchong.common.ApiResponse;
 import com.ajouchong.dto.AddMemberRequestDto;
 import com.ajouchong.entity.Member;
+import com.ajouchong.exception.DuplicateEmailException;
 import com.ajouchong.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,11 @@ public class MemberController {
 
     @PostMapping("/signup")
     public ApiResponse<Member> signup(@RequestBody AddMemberRequestDto requestDto) {
-        Member member = memberService.save(requestDto);
-        return new ApiResponse<>(1, "회원가입에 성공했습니다.", member);
+        try {
+            Member member = memberService.save(requestDto);
+            return new ApiResponse<>(1, "회원가입에 성공했습니다.", member);
+        } catch (DuplicateEmailException e) {
+            return new ApiResponse<>(0, "이미 존재하는 이메일입니다.", null);
+        }
     }
 }
--- src/main/java/com/ajouchong/service/MemberService.java
+++ src/main/java/com/ajouchong/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ajouchong.service;
 
 import com.ajouchong.dto.AddMemberRequestDto;
 import com.ajouchong.entity.Member;
+import com.ajouchong.exception.DuplicateEmailException;
 import com.ajouchong.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,10 @@ public class MemberService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Member save(AddMemberRequestDto requestDto) {
+        if (memberRepository.existsByEmail(requestDto.getEmail())) {
+            throw new DuplicateEmailException("이미 존재하는 이메일입니다.");
+        }
+
         Member member = Member.builder()
                 .name(requestDto.getName())
                 .email(requestDto.getEmail())
