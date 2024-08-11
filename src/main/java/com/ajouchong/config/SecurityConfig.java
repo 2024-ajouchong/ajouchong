411db60f577a7b24bb3f5fda06d4852bcc744ef1
유수정 <ysj120808@gmail.com>
2024-08-12 00:30:50 +0900

    feat: change config

--- src/main/java/com/ajouchong/config/SecurityConfig.java
+++ src/main/java/com/ajouchong/config/SecurityConfig.java
@@ -1,2 +1,54 @@
-package com.ajouchong.config;public class SecurityConfig {
+package com.ajouchong.config;
+
+import com.ajouchong.jwt.JwtAuthenticationFilter;
+import com.ajouchong.jwt.JwtTokenProvider;
+import com.ajouchong.service.MemberDetailService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final MemberDetailService memberDetailService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider, memberDetailService, authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)));
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll() // 모든 요청 허용 API
+                        .anyRequest().authenticated() // 그 외의 요청은 인증 필요
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
