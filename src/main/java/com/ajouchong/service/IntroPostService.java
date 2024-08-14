0d063c4ba455e32b1275fb6bccc1c6b5fea571c8
유수정 <ysj120808@gmail.com>
2024-08-14 23:40:12 +0900

    feat: #11 add IntroPost Service

--- src/main/java/com/ajouchong/service/IntroPostService.java
+++ src/main/java/com/ajouchong/service/IntroPostService.java
@@ -1,2 +1,28 @@
-package com.ajouchong.service;public class IntroPostService {
+package com.ajouchong.service;
+
+import com.ajouchong.entity.IntroPost;
+import com.ajouchong.entity.IntroPostPageName;
+import com.ajouchong.repository.IntroPostRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class IntroPostService {
+    private IntroPostRepository introPostRepository;
+
+    public IntroPost uploadImage(IntroPostPageName page, String imageUrl) {
+        IntroPost post = new IntroPost();
+        post.setPage(page);
+        post.setImageUrl(imageUrl);
+        post.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        return introPostRepository.save(post);
+    }
+
+    public List<IntroPost> getPhotosByPage(IntroPostPageName page) {
+        return introPostRepository.findByPage(page);
+    }
+
 }
