c61792909aaf5a33dbc29bbd2256c5448f70bb5e
유수정 <ysj120808@gmail.com>
2024-08-15 19:00:13 +0900

    chore: #18 change Notice post field

--- src/main/java/com/ajouchong/entity/NoticePost.java
+++ src/main/java/com/ajouchong/entity/NoticePost.java
@@ -5,10 +5,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 @Entity
 public class NoticePost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long nPost_id;
 
+    private String np_title;
+    private String np_content;
+    private int np_userLike_cnt;
+    private Long user_id;
+    private Date np_createTime;
+    private Date np_updateTime;
+    private int np_hit_cnt;
+    private boolean np_isReplied;
 }
