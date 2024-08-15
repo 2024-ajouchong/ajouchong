b8d3a05ff6f03ac71ef1d44751ffbafa287b1b85
유수정 <ysj120808@gmail.com>
2024-08-15 21:54:23 +0900

    feat: #18 add notice post controller

--- src/main/java/com/ajouchong/controller/admin/NoticePostAdminController.java
+++ src/main/java/com/ajouchong/controller/admin/NoticePostAdminController.java
@@ -1,2 +1,35 @@
-package com.ajouchong.controller.admin;public class NoticePostController {
+package com.ajouchong.controller.admin;
+
+import com.ajouchong.common.ApiResponse;
+import com.ajouchong.dto.NoticePostResponseDto;
+import com.ajouchong.dto.NoticePostUploadDto;
+import com.ajouchong.service.NoticePostService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notice")
+public class NoticePostAdminController {
+    private final NoticePostService noticePostService;
+
+    public NoticePostAdminController(NoticePostService noticePostService) {
+        this.noticePostService = noticePostService;
+    }
+
+    @PostMapping
+    public ApiResponse<NoticePostResponseDto> uploadNoticePost(
+            @RequestBody NoticePostUploadDto dto,
+            @RequestHeader("Authorization") String authorizationHeader) {
+
+        String token = authorizationHeader.substring(7);
+        NoticePostResponseDto savedNoticePost = noticePostService.saveNoticePost(dto, token);
+
+        return new ApiResponse<>(1, "게시글 업로드 성공", savedNoticePost);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<Void> deleteNoticePost(@PathVariable Long id) {
+        noticePostService.deleteNoticePost(id);
+        return new ApiResponse<>(1, id + "번 게시글 삭제 성공", null);
+    }
 }
--- src/main/java/com/ajouchong/controller/user/NoticePostUserController.java
+++ src/main/java/com/ajouchong/controller/user/NoticePostUserController.java
@@ -1,2 +1,36 @@
-package com.ajouchong.controller.user;public class NoticePostUserController {
+package com.ajouchong.controller.user;
+
+import com.ajouchong.common.ApiResponse;
+import com.ajouchong.dto.NoticePostResponseDto;
+import com.ajouchong.entity.NoticePost;
+import com.ajouchong.service.NoticePostService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/notice")
+public class NoticePostUserController {
+    private final NoticePostService noticePostService;
+
+    public NoticePostUserController(NoticePostService noticePostService) {
+        this.noticePostService = noticePostService;
+    }
+
+    @GetMapping
+    public ApiResponse<List<NoticePostResponseDto>> getAllNoticePosts() {
+        List<NoticePostResponseDto> posts = noticePostService.getAllNoticePosts();
+        return new ApiResponse<>(1, "모든 게시글 조회 성공", posts);
+    }
+
+    // 특정 게시물 조회
+    @GetMapping("/{id}")
+    public ApiResponse<NoticePostResponseDto> getNoticePostById(@PathVariable Long id) {
+        NoticePostResponseDto post = noticePostService.getNoticePostById(id);
+        return new ApiResponse<>(1, id + "번 게시글 조회 성공", post);
+    }
 }
