6489011badb708cda2a22031e3380fb85fb7eb3c
유수정 <ysj120808@gmail.com>
2024-08-15 21:53:52 +0900

    feat: #18 add notice post response & upload dto

--- src/main/java/com/ajouchong/dto/NoticePostUploadDto.java
+++ src/main/java/com/ajouchong/dto/NoticePostUploadDto.java
@@ -1,2 +1,13 @@
-package com.ajouchong.dto;public class NoticePostUploadDto {
+package com.ajouchong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class NoticePostUploadDto {
+    private String title;
+    private String content;
+    private List<String> imageUrls;
 }
