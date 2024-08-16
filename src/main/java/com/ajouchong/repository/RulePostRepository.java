349c9310b6e97ccb8a90bcd0da17bda0722a0899
유수정 <ysj120808@gmail.com>
2024-08-16 18:47:04 +0900

    feat: #24 add rulePost repository

---
+++ src/main/java/com/ajouchong/repository/RulePostRepository.java
@@ -0,0 +1,8 @@
+package com.ajouchong.repository;
+
+import com.ajouchong.entity.RulePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RulePostRepository extends JpaRepository<RulePost, Long> {
+
+}
