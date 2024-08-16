649cb878a10290bc434ebdd3d4ab728743e0ee64
유수정 <ysj120808@gmail.com>
2024-08-16 19:47:19 +0900

    feat: #24 add rulePost Dto

--- src/main/java/com/ajouchong/dto/RulePostRequestDto.java
+++ src/main/java/com/ajouchong/dto/RulePostRequestDto.java
@@ -1,2 +1,13 @@
-package com.ajouchong.dto;public class RulePostRequestDto {
+package com.ajouchong.dto;
+
+import com.ajouchong.entity.enumClass.RuleType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RulePostRequestDto {
+    private String rpTitle;
+    private String rpContent;
+    private String attachmentUrl;
+    private RuleType ruleType;
 }
