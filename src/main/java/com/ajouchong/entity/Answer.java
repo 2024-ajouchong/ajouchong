638dc7c9a7f7737667c39188ad101400165a6c4b
유수정 <ysj120808@gmail.com>
2024-09-04 22:05:05 +0900

    feat: #26 add Answer entity

--- src/main/java/com/ajouchong/entity/Answer.java
+++ src/main/java/com/ajouchong/entity/Answer.java
@@ -1,2 +1,35 @@
-package com.ajouchong.entity;public class Answer {
+package com.ajouchong.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long answerId;
+
+    @OneToOne
+    @JoinColumn(name = "qPostId", referencedColumnName = "qPostId")
+    private QnaPost qnaPost;
+
+    private String content;
+
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createTime = LocalDateTime.now();
+        this.updateTime = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updateTime = LocalDateTime.now();
+    }
 }
