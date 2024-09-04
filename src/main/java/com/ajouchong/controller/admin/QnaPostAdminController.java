f0b5a56945abe5851ab40e6f5299cc4294d3ac50
유수정 <ysj120808@gmail.com>
2024-09-04 22:43:28 +0900

    feat: #26 add qna post controller

--- src/main/java/com/ajouchong/controller/admin/QnaPostAdminController.java
+++ src/main/java/com/ajouchong/controller/admin/QnaPostAdminController.java
@@ -1,2 +1,40 @@
-package com.ajouchong.controller.admin;public class QnaPostAdminController {
+package com.ajouchong.controller.admin;
+
+import com.ajouchong.common.ApiResponse;
+import com.ajouchong.entity.QnaPost;
+import com.ajouchong.service.QnaPostService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/qna")
+public class QnaPostAdminController {
+    private final QnaPostService qnaPostService;
+
+    public QnaPostAdminController(QnaPostService qnaPostService) {
+        this.qnaPostService = qnaPostService;
+    }
+
+    @PostMapping("/{postId}/answer")
+    public ApiResponse<QnaPost> addAnswer(@PathVariable Long postId, @RequestParam String answerContent) {
+        QnaPost updatedPost = qnaPostService.addAnswer(postId, answerContent);
+        return new ApiResponse<>(1, "답변이 게시되었습니다.", updatedPost);
+    }
+
+    @PutMapping("/{postId}/answer")
+    public ApiResponse<QnaPost> changeAnswer(@PathVariable Long postId, @RequestParam String updatedAnswerContent) {
+        QnaPost updatedPost = qnaPostService.changeAnswer(postId, updatedAnswerContent);
+        return new ApiResponse<>(1, "답변이 수정되었습니다.", updatedPost);
+    }
+
+    @DeleteMapping("/{postId}/answer")
+    public ApiResponse<QnaPost> deleteAnswer(@PathVariable Long postId) {
+        QnaPost updatedPost = qnaPostService.deleteAnswer(postId);
+        return new ApiResponse<>(1, "답변이 삭제되었습니다.", updatedPost);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ApiResponse<Void> deletePost(@PathVariable Long postId) {
+        qnaPostService.deletePost(postId);
+        return new ApiResponse<>(1, "게시글이 삭제되었습니다.", null);
+    }
 }
--- src/main/java/com/ajouchong/controller/user/QnaPostController.java
+++ src/main/java/com/ajouchong/controller/user/QnaPostController.java
@@ -1,9 +1,49 @@
 package com.ajouchong.controller.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ajouchong.common.ApiResponse;
+import com.ajouchong.entity.QnaPost;
+import com.ajouchong.service.QnaPostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/qna")
 public class QnaPostController {
+    private final QnaPostService qnaPostService;
+
+    public QnaPostController(QnaPostService qnaPostService) {
+        this.qnaPostService = qnaPostService;
+    }
+
+    @PostMapping()
+    public ApiResponse<QnaPost> createPost(@RequestParam String title, @RequestParam String content) {
+        QnaPost post = qnaPostService.createPost(title, content);
+        return new ApiResponse<>(1, "게시글이 게시되었습니다.", post);
+    }
+
+    @GetMapping()
+    public ApiResponse<List<QnaPost>> getAllPosts() {
+        List<QnaPost> allPosts = qnaPostService.getAllPosts();
+        return new ApiResponse<>(1, "전체 게시글 목록 조회 성공", allPosts);
+    }
+
+    @GetMapping("{postId}")
+    public ApiResponse<Optional<QnaPost>> getPostById(@PathVariable Long postId) {
+        Optional<QnaPost> post = qnaPostService.getPostById(postId);
+        qnaPostService.incrementHitCount(postId);
+        if (post.isPresent()) {
+            qnaPostService.incrementHitCount(postId);
+            return new ApiResponse<>(1, postId + "번 게시글 조회 성공", post);
+        } else {
+            return new ApiResponse<>(0, "게시글을 찾을 수 없습니다.", null);
+        }
+    }
+
+    @PostMapping("/{postId}/like")
+    public ApiResponse<Void> incrementLikeCount(@PathVariable Long postId) {
+        qnaPostService.incrementUserLikeCount(postId);
+        return new ApiResponse<>(1, postId + "번 게시글 좋아요 성공", null);
+    }
 }
